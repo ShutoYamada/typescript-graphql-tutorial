7c7fb778e5f616c95ff2500e5a0b6abcc298323f
ShutoYamada <stym1122@gmail.com>
2020-10-29 23:15:00 +0900

    Update index.ts

--- src/mutation/index.ts
+++ src/mutation/index.ts
@@ -20,7 +20,6 @@ const resolvers = {
     },
     Mutation: {
         create: (root, args : { param: CreateParam }): Chapter => {
-            console.log("args --> ", args);
             // (省略)引数を使ってDBに書き込み...
             const result: Chapter = {
                 ...args?.param,
@@ -51,29 +50,3 @@ const server = new GraphQLServer(
 server.start(() => {
     console.log(`Server is running on http://localhost:4000`)
 });
-
-
-`
-Query
-# Write your query or mutation here
-mutation($param: UpdateParam!) {
-  update(param: $param) {
-    no
-    name
-    version
-    original
-    postDate
-  }
-}
-
-QueryValiables
-{
-    "param": {
-      "no": 4,
-      "name": "Mutationを使って追加・更新を行う",
-      "version": "1.0.0",
-      "original": true,
-      "postDate": "2020-10-29T:12:00:00Z"
-    }
-  }
-`
